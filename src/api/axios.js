c88e0bae0e66eecc92cc882c82cac281d8d14531
Mohan Raja Ram R K <mohanram2812@gmail.com>
2025-12-26 18:34:23 +0530

    Connected frontend to live backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://blogapp-backend-ckl7.onrender.com/",
 });
+
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
